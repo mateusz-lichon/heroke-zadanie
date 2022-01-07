fe0a7ae2d77e91db0817c1f21d0ef6ae9e06fd7d
mateusz-lichon <matik-lichon@wp.pl>
2022-01-07 22:40:04 +0100

    commitcik

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 const data = { imie: "Mateusz", nazwisko: "Lichoń", klasa: "3I1", grupa: "2" }
 
+app.get("/", function (req, res) {
+    res.send("Wpisz /data w URL")
+})
 app.get("/data", function (req, res) {
     res.send(data)
 })
